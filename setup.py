03311cfce9ac31fafe1614d7c1b76dc47eddeb33
Sébastien Copin <cosad3s@outlook.com>
2025-08-28 23:14:34 +0200

    Up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from os import path
 import io
 
-VERSION = '1.1.7' 
+VERSION = '1.1.8' 
 DESCRIPTION = 'Postleaks'
 
 pwd = path.abspath(path.dirname(__file__))
